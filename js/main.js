4d850358ca6f1bd65fe69c00d9c1e2bd72b754d7
Alizadeee <cavid.elizade.2001@mail.ru>
2023-02-26 11:19:52 +0400

    added files

---
+++ js/main.js
@@ -0,0 +1,13 @@
+//select
+function select(){
+    document.getElementById('Up').style.display = 'block'
+    document.getElementById('Down').style.display = 'none'
+}
+select()
+
+function selectBlur(){
+    document.getElementById('Up').style.display = 'none'
+    document.getElementById('Down').style.display = 'block'
+    
+}
+selectBlur()
